5f65d697294219eaad6404bdf16bc91cc8e72b7b
lewis-chinery <lewis.chinery@hotmail.co.uk>
2019-05-02 09:00:38 +0100

    Check button by changing text in dummy TextView

--- app/src/main/java/com/example/lewis/vocabbuddy/MainActivity.java
+++ app/src/main/java/com/example/lewis/vocabbuddy/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.lewis.vocabbuddy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button recordButton;
+    TextView recordTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +17,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recordButton = (Button) findViewById(R.id.record_button);
+        recordTextView = (TextView) findViewById(R.id.record_textview);
+    }
+
+    public void changeText(View view) {
+        recordTextView.setText("Record button pressed successfully");
     }
 }

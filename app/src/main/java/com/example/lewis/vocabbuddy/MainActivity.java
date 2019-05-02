dbed32d0224fbc820b73f66f4e40b04e72f56fd4
lewis-chinery <lewis.chinery@hotmail.co.uk>
2019-05-02 08:11:34 +0100

    Delcare and assign record button in MainActivity.java

--- app/src/main/java/com/example/lewis/vocabbuddy/MainActivity.java
+++ app/src/main/java/com/example/lewis/vocabbuddy/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.lewis.vocabbuddy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button recordButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recordButton = (Button) findViewById(R.id.record_button);
     }
 }

de84e7eb080849e0e45be5bf2b4bc5e0e4b324ba
lewis-chinery <lewis.chinery@hotmail.co.uk>
2019-05-08 16:48:23 +0100

    Add ListVocabActivity

---
+++ app/src/main/java/com/example/lewis/vocabbuddy/ListVocabActivity.java
@@ -0,0 +1,13 @@
+package com.example.lewis.vocabbuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListVocabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_vocab);
+    }
+}

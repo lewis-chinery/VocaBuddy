cdee5ceac530b476cb01dbdf72c3adeeb568310c
lewis-chinery <lewis.chinery@hotmail.co.uk>
2019-05-09 07:57:52 +0100

    Add convertTextToArray method to Vocab class

--- app/src/main/java/com/example/lewis/vocabbuddy/Vocab.java
+++ app/src/main/java/com/example/lewis/vocabbuddy/Vocab.java
@@ -2,6 +2,9 @@ package com.example.lewis.vocabbuddy;
 
 public class Vocab {
 
-    
+    public String[] convertTextToArray (String inputText) {
+        String[] outputArray = inputText.split(" ");
+        return outputArray;
+    }
 
 }

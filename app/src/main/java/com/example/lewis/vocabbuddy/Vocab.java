b4e1179f1f69234f707b96a41bbfb938526426d9
lewis-chinery <lewis.chinery@hotmail.co.uk>
2019-05-18 11:03:15 +0100

    Add populateHashMapWithWords method

--- app/src/main/java/com/example/lewis/vocabbuddy/Vocab.java
+++ app/src/main/java/com/example/lewis/vocabbuddy/Vocab.java
@@ -1,10 +1,20 @@
 package com.example.lewis.vocabbuddy;
 
+import java.util.HashMap;
+
 public class Vocab {
 
     public static String[] convertTextToArray (String inputText) {
         String[] outputArray = inputText.split(" ");
         return outputArray;
+    }
+
+    public static HashMap<String, Integer> populateHashMapWithWords (String[] inputArray) {
+        HashMap<String, Integer> HashMapWithZeroValues = new HashMap<String, Integer>();
+        for (String word : inputArray) {
+            HashMapWithZeroValues.put(word, 0);
+        }
+        return HashMapWithZeroValues;
     }
 
 }
